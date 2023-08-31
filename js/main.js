034d3dc5eae48c86ab8ae706d089afdd33b4ea50
shamimislam <134716211+shamim-devoloper@users.noreply.github.com>
2023-08-31 14:18:27 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// Header Scroll
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("header-scrolled");
+    }else{
+        nav.classList.remove("header-scrolled");
+    }
+} 
+// nav hide 
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
+// Scroll Back To Top 
+function scrollTopBack(){
+    let scrollTopButton = document.querySelector("#scrollUp");
+    window.onscroll = function (){
+        var scroll = document.documentElement.scrollTop;
+        if(scroll >= 250){
+            scrollTopButton.classList.add('scrollActive');
+        } else{
+            scrollTopButton.classList.remove('scrollActive');
+        }
+    }
+}
+scrollTopBack();
